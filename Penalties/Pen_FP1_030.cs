93e96e7ec2e35fcf3e866060e7d56a1d49adcb92
xytrix <xytrix@users.noreply.github.com>
2015-09-23 02:28:42 -0400

    Add play penalty for Loatheb on an empty board, or a board not threatened by AoE, since it would just be a vanilla 5/5 at that point.

--- Penalties/Pen_FP1_030.cs
+++ Penalties/Pen_FP1_030.cs
@@ -8,6 +8,10 @@ namespace HREngine.Bots
 	{
 		public override float getPlayPenalty(Playfield p, Handmanager.Handcard hc, Minion target, int choice, bool isLethal)
 		{
+            // penalty for playing Loatheb as a vanilla 5/5, i.e. on an empty board (should hold for offensive finishing or preventing AoE/removal)
+            if (p.ownHero.Hp + p.ownHero.armor > 15 && p.enemyMinions.Count == 0 && p.ownMinions.Count < 3)
+                return 10;
+
 			return 0;
 		}
 	}
